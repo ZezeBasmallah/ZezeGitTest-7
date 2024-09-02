b2fec2014244f4eccd0def0bce6c5d0b190ac35a
ZezeBasmallah <zezezeko19@gmail.com>
2024-09-02 17:05:10 +0700

    nambah paeagraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>taik</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem, voluptatibus blanditiis possimus et, quos, culpa porro excepturi labore ducimus inventore magnam ut amet odit. Vitae numquam facere incidunt rerum. Sint nam est iusto iste? Repudiandae libero harum nisi quasi, eius iusto consequuntur nesciunt dignissimos distinctio aliquid eligendi quisquam deleniti, nostrum officiis ea culpa! Rerum iste cum magni laborum sunt minus accusamus alias fuga sequi ab eum dolorem vero eligendi magnam consectetur, quibusdam nisi deserunt officia, incidunt maiores ullam tempora? Asperiores, tempore! Aliquam earum provident officiis ratione laudantium, magnam similique soluta ex deserunt tempora magni sed dignissimos odio sint omnis?F</p>
 </body>
 </html>
